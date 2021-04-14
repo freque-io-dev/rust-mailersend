83daff81ea56df55bcf42c6fb39464e1a450a16d
meh <meh@schizofreni.co>
2021-04-14 18:52:38 +0200

    refactor: move some types around

--- src/email.rs
+++ src/email.rs
@@ -27,53 +27,53 @@ pub struct Substitution {
 	pub value: String,
 }
 
+#[derive(Clone, Serialize, Debug)]
+pub struct Message {
+	pub from: Recipient,
+	pub to: Vec<Recipient>,
+	#[serde(default)]
+	pub cc: Vec<Recipient>,
+	#[serde(default)]
+	pub bcc: Vec<Recipient>,
+	#[serde(default)]
+	pub reply_to: Option<Recipient>,
+	pub subject: String,
+	pub text: String,
+	pub html: String,
+	#[serde(default)]
+	pub attachments: Vec<Attachment>,
+	#[serde(default)]
+	pub tags: Vec<String>,
+	#[serde(default)]
+	pub variables: Vec<Variable>,
+}
+
+#[derive(Clone, Serialize, Debug)]
+pub struct Template {
+	pub to: Vec<Recipient>,
+	#[serde(default)]
+	pub cc: Vec<Recipient>,
+	#[serde(default)]
+	pub bcc: Vec<Recipient>,
+	#[serde(default)]
+	pub reply_to: Option<Recipient>,
+	pub template_id: String,
+	#[serde(default)]
+	pub attachments: Vec<Attachment>,
+	#[serde(default)]
+	pub tags: Vec<String>,
+	#[serde(default)]
+	pub variables: Vec<Variable>,
+}
+
 pub mod send {
 	use serde::Serialize;
 
 	#[derive(Clone, Serialize, Debug)]
 	#[serde(untagged)]
 	pub enum Request {
-		Message(Message),
-		Template(Template),
-	}
-
-	#[derive(Clone, Serialize, Debug)]
-	pub struct Message {
-		pub from: super::Recipient,
-		pub to: Vec<super::Recipient>,
-		#[serde(default)]
-		pub cc: Vec<super::Recipient>,
-		#[serde(default)]
-		pub bcc: Vec<super::Recipient>,
-		#[serde(default)]
-		pub reply_to: Option<super::Recipient>,
-		pub subject: String,
-		pub text: String,
-		pub html: String,
-		#[serde(default)]
-		pub attachments: Vec<super::Attachment>,
-		#[serde(default)]
-		pub tags: Vec<String>,
-		#[serde(default)]
-		pub variables: Vec<super::Variable>,
-	}
-
-	#[derive(Clone, Serialize, Debug)]
-	pub struct Template {
-		pub to: Vec<super::Recipient>,
-		#[serde(default)]
-		pub cc: Vec<super::Recipient>,
-		#[serde(default)]
-		pub bcc: Vec<super::Recipient>,
-		#[serde(default)]
-		pub reply_to: Option<super::Recipient>,
-		pub template_id: String,
-		#[serde(default)]
-		pub attachments: Vec<super::Attachment>,
-		#[serde(default)]
-		pub tags: Vec<String>,
-		#[serde(default)]
-		pub variables: Vec<super::Variable>,
+		Message(super::Message),
+		Template(super::Template),
 	}
 }
 
@@ -98,11 +98,11 @@ impl Api {
 		Ok(response.headers()["X-Message-Id"].to_str()?.into())
 	}
 
-	pub async fn message(&self, request: send::Message) -> crate::Result<MessageId> {
+	pub async fn message(&self, request: Message) -> crate::Result<MessageId> {
 		self.send(send::Request::Message(request)).await
 	}
 
-	pub async fn template(&self, request: send::Template) -> crate::Result<MessageId> {
+	pub async fn template(&self, request: Template) -> crate::Result<MessageId> {
 		self.send(send::Request::Template(request)).await
 	}
 }
